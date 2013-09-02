d16479117117550feda0b0da5f8d08afcea1e8b2
sicklebot <sicklebot@ircd.(none)>
2013-09-02 20:43:40 +0000

    added the configuration options

---
+++ config.pm
@@ -0,0 +1,26 @@
+package scriptcfg;
+
+use strict;
+
+##FILES WHICH USE THIS CONFIGURATION FILE
+#ident.pl
+#np.pl
+#rt.pl
+#sr.pl
+#ul.pl
+
+#ident.pl
+%id_authorizations = ('netname' => ["username","servicename", "password"]);
+
+#np.pl
+$np_MPD_SERVER = "127.0.0.1";
+$np_MPD_PORT   = "6600";
+
+#rt.pl
+$rt_SCRIPT_DIR = "~/scripts/";
+
+#sr.pl
+%sr_ALIASES = ( 'path-to-mountpoint' => ['array', 'of', 'aliases']);
+
+#ul.pl
+$ul_DBPATH = "~/.irssi";
