6579eee4019030efd0708520dae7de1fd4409b46
loridcon <loridcon@deepmagic>
2014-02-12 18:35:18 -0500

    Updated script_config.pm with nowplaying-vlc related variables

--- script_config.pm
+++ script_config.pm
@@ -17,6 +17,16 @@ our %id_authorizations = ('netname' => ["username","servicename", "password"]);
 our $np_MPD_SERVER = "127.0.0.1";
 our $np_MPD_PORT   = "6600";
 
+#enables or disables the /npv and /nprv commands
+our $np_VLC = 1;
+
+#full path to the VLC xml file which will generate the XML data you want
+our $np_VLC_URL = "http://127.0.0.1:8080/requests/irssi.xml";
+
+#password for accessing the VLC web interface
+our $np_VLC_PASS = "password"
+
+
 #rt.pl
 our $rt_SCRIPT_DIR = "~/scripts/";
 
