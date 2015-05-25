7a2320b9a311b086a8db3fd761d0cf4621a39a50
alandrees <alandrees@theselves.com>
2015-05-25 13:53:06 -0400

    whitespace fixes

--- sd.pl
+++ sd.pl
@@ -6,20 +6,20 @@ use Irssi qw(command_bind signal_add);
 use Module::Refresh;
 $VERSION = '1.00';
 %IRSSI = (
-        authors         => 'Alan Drees',
-        contact         => 'alandrees@theselves.com',
-        name            => 'Self Defence',
-        description     => 'keep deepmagic somewhat protected from violators.',
-        license         => 'GPLv3',
-);
+    authors         => 'Alan Drees',
+    contact         => 'alandrees@theselves.com',
+    name            => 'Self Defence',
+    description     => 'keep deepmagic somewhat protected from violators.',
+    license         => 'GPLv3',
+    );
 
 sub _self_defence{
-        my ($server, $msg, $nick, $address, $target) = @_;
-        my ($selfnick);
-	$selfnick = lc($server->{nick});
-        if(lc($msg) =~ $selfnick){
-                $server->command('action '.$target.' responds with an equal and opposite force!');
-        }
+    my ($server, $msg, $nick, $address, $target) = @_;
+    my ($selfnick);
+    $selfnick = lc($server->{nick});
+    if(lc($msg) =~ $selfnick){
+	$server->command('action '.$target.' responds with an equal and opposite force!');
+    }
 }
 
 signal_add("message irc action", "_self_defence");
