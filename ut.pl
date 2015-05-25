58883f03d5ddc72d4e4d675d7d531ffd5d4ec611
alandrees <alandrees@theselves.com>
2015-05-25 13:53:42 -0400

    whitespace fixes

--- ut.pl
+++ ut.pl
@@ -8,12 +8,12 @@ use Module::Refresh;
 $VERSION = '1.00';
 
 %IRSSI = (
-        authors         => 'Alan Drees',
-        contact         => 'alandrees@theselves.com',
-        name            => 'uptime',
-        description     => 'irssi uptime output',
-        license         => 'GPLv2',
-);
+    authors         => 'Alan Drees',
+    contact         => 'alandrees@theselves.com',
+    name            => 'uptime',
+    description     => 'irssi uptime output',
+    license         => 'GPLv2',
+    );
 
 
 sub _self_uptime{
@@ -48,14 +48,14 @@ sub calc_uptime{
 
     $response = readpipe($script_config::uptime_command);
     $elapsed = int($response);
-	
+
     $days = int($elapsed / 86400); #total days
     $weeks = int($days / 7); #weeks
     $days = $days % 7; #days less than a week
     $hours = int(($elapsed % 86400) / 3600); #hours
     $minutes = int((($elapsed % 86400) % 3600) / 60); #minutes
     $seconds = int(((($elapsed % 86400) % 3600) % 60));
-	
+
     $response = "";
 
     if($weeks > 0){
@@ -65,7 +65,7 @@ sub calc_uptime{
 	    $response .= $weeks . " Week ";
 	}
     }
-    
+
     if($days > 0){
 	if($days > 1){
 	    $response .= $days . " Days ";
@@ -81,7 +81,7 @@ sub calc_uptime{
 	    $response .= $hours . " Hour ";
 	}
     }
-    
+
     if($minutes > 0){
 	if($minutes > 1){
 	    $response .= $minutes . " Minutes ";
@@ -89,7 +89,7 @@ sub calc_uptime{
 	    $response .= $minutes . " Minute ";
 	}
     }
-    
+
     if($seconds > 0){
 	if($seconds > 1){
 	    $response .= $seconds . " Seconds";
