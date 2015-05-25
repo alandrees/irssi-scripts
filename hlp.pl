ced1fa7c03e563d971d247e1c62286ed12778eee
alandrees <alandrees@theselves.com>
2015-05-25 13:51:40 -0400

    whitespace fixes

--- hlp.pl
+++ hlp.pl
@@ -13,11 +13,11 @@ use Module::Refresh;
 
 $VERSION = '0.02';
 %IRSSI = (
-        authors         => 'Alan Drees',
-        contact         => 'alandrees@theselves.com',
-        name            => 'HELP!',
-        description     => 'Provides a command listing',
-        license         => 'GPLv3',
+    authors => 'Alan Drees',
+    contact => 'alandrees@theselves.com',
+    name    => 'HELP!',
+    description     => 'Provides a command listing',
+    license => 'GPLv3',
     );
 
 
@@ -25,8 +25,8 @@ $VERSION = '0.02';
 sub _help{
     my($server, $msg, $nick, $address, $target) = @_;
 
-    my @arguments = split(' ',$msg); 
-    
+    my @arguments = split(' ',$msg);
+
     if(lc($arguments[0]) eq '!help'){
 	$server->command('NOTICE '.$nick.' !ip <dns name> - Resolves the IP address for a dns name.');
 	$server->command('NOTICE '.$nick.' !uptime - Current system uptime.');
@@ -35,7 +35,7 @@ sub _help{
 	$server->command('NOTICE '.$nick.' !rtorrent - Current rtorrent throughput.');
 	$server->command('NOTICE '.$nick.' !iostat [device] - Current disk IO usage.');
 	$server->command('NOTICE '.$nick.' !dice <options> - Randomly pick of the space-delimited options.');
-	$server->command('NOTICE '.$nick.' !net - Current network throughput.');       
+	$server->command('NOTICE '.$nick.' !net - Current network throughput.');
 	$server->command('NOTICE '.$nick.' !help - This help text.');
 	$server->command('NOTICE '.$nick.' !futurama - Random Futurama Quote.');
     }
