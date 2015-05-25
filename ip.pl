af4d92d56bf1b593becbd980129fa650bae1e5ad
alandrees <alandrees@theselves.com>
2015-05-25 13:52:05 -0400

    whitespace fixes

--- ip.pl
+++ ip.pl
@@ -6,12 +6,12 @@ use Irssi qw(command_bind signal_add);
 use Module::Refresh;
 $VERSION = '0.50';
 %IRSSI = (
-        authors         => 'Alan Drees',
-        contact         => 'alandrees@theselves.com',
-        name            => 'IP resolving',
-        description     => 'IP Resolving for DNS names',
-        license         => 'GPLv3',
-);
+    authors         => 'Alan Drees',
+    contact         => 'alandrees@theselves.com',
+    name            => 'IP resolving',
+    description     => 'IP Resolving for DNS names',
+    license         => 'GPLv3',
+    );
 
 sub _resolve{
     my($server, $msg, $nick, $address, $target) = @_;
@@ -20,7 +20,7 @@ sub _resolve{
     my @arguments = split(' ', $msg);
 
     if(lc($arguments[0]) eq '!ip'){
-	
+
 	$response = readpipe('dig +short "'.$arguments[1].'" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"');
 	$server->command('MSG '.$target.' '.''.$arguments[1].":4 ".$response);
     }
@@ -32,4 +32,3 @@ signal_add("message public", "_resolve");
 #all of irssi just to test a new configuration variable
 my $refresher = Module::Refresh->new;
 $refresher->refresh_module('script_config.pm');
-
