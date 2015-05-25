9c273f45b0fe643d0dad764f0be83064682ffd4e
alandrees <alandrees@theselves.com>
2015-05-25 13:53:37 -0400

    whitespace fixes

--- up.pl
+++ up.pl
@@ -8,30 +8,30 @@ use Irssi qw(command_bind signal_add);
 use Module::Refresh;
 $VERSION = '1.01';
 %IRSSI = (
-        authors         => 'Alan Drees',
-        contact         => 'alandrees@theselves.com',
-        name            => 'ups status',
-        description     => 'get the current wattage draw of a UPS',
-        license         => 'GPLv2',
-);
+    authors         => 'Alan Drees',
+    contact         => 'alandrees@theselves.com',
+    name            => 'ups status',
+    description     => 'get the current wattage draw of a UPS',
+    license         => 'GPLv2',
+    );
 
 sub upsw{
-        my ($server, $msg, $nick, $address, $target) = @_;
-        my ($draw, $response);
-	my @arguments = split(' ',$msg);
+    my ($server, $msg, $nick, $address, $target) = @_;
+    my ($draw, $response);
+    my @arguments = split(' ',$msg);
 
-	if(lc($arguments[0]) eq '!ups'){
-	    if($arguments[1] ne ''){
-		while ( my ($key, $value) = each (%script_config::ups_commands) ){
-		    if(lc($key) =~ lc($arguments[1])){
-			$draw = readpipe($value);
-			$response = "8".$key.":4 ".$draw.'W';
-		    }
+    if(lc($arguments[0]) eq '!ups'){
+	if($arguments[1] ne ''){
+	    while ( my ($key, $value) = each (%script_config::ups_commands) ){
+		if(lc($key) =~ lc($arguments[1])){
+		    $draw = readpipe($value);
+		    $response = "8".$key.":4 ".$draw.'W';
 		}
 	    }
+	}
 
-	    $server->command('msg '.$target.' '.$response);
-        }
+	$server->command('msg '.$target.' '.$response);
+    }
 }
 
 signal_add("message public", "upsw");
@@ -40,4 +40,3 @@ signal_add("message public", "upsw");
 #all of irssi just to test a new configuration variable
 my $refresher = Module::Refresh->new;
 $refresher->refresh_module('script_config.pm');
-
