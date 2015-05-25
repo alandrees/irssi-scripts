3f4c612a35395cab0c2da43748084ee62a933be8
alandrees <alandrees@theselves.com>
2015-05-25 13:53:00 -0400

    whitespace fixes

--- rt.pl
+++ rt.pl
@@ -7,12 +7,12 @@ use Irssi qw(command_bind signal_add);
 use Module::Refresh;
 $VERSION = '1.00';
 %IRSSI = (
-        authors         => 'Alan Drees',
-        contact         => 'alandrees@theselves.com',
-        name            => 'rtorrent',
-        description     => 'get the current rtorrent throughput',
-        license         => 'GPLv3',
-);
+    authors => 'Alan Drees',
+    contact => 'alandrees@theselves.com',
+    name    => 'rtorrent',
+    description     => 'get the current rtorrent throughput',
+    license => 'GPLv3',
+    );
 
 
 #set this to the location of the rtorrent-rate.sh script
@@ -21,11 +21,11 @@ sub _rtorrent{
     my($response, $dl, $ul);
 
     my @arguments = split(' ',$msg);
-    
+
     if(lc($arguments[0]) eq '!rtorrent'){
 	$dl = readpipe($script_config::rt_SCRIPT_DIR."rtorrent-rate.sh download_rate");
-	$ul = readpipe($script_config::rt_SCRIPT_DIR."rtorrent-rate.sh upload_rate");   
-    	$server->command("MSG ".$target." Upload: ".$ul." "."Download: ".$dl); 
+	$ul = readpipe($script_config::rt_SCRIPT_DIR."rtorrent-rate.sh upload_rate");
+	$server->command("MSG ".$target." Upload: ".$ul." "."Download: ".$dl);
     }
 }
 
